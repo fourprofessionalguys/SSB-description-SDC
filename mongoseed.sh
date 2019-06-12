501f22131b4672c8efbb5d3deaf45cd23cba8e33
Srujan Bethi <ssbethi@gmail.com>
2019-06-11 19:51:50 -0600

    Setup 4 mongo EC2 instances and all the connections in node

--- mongoseed.sh
+++ mongoseed.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-mongoimport --db descriptions --type csv --collection descriptiondata --headerline --file /Users/srujan/SSB-description-SDC/descriptionData.csv
-mongoimport --db descriptions --type csv --collection shoeslist --headerline --file /Users/srujan/SSB-description-SDC/shoeData.csv
+mongoimport --uri "mongodb://srujan:Rosie123@ec2-13-57-215-108.us-west-1.compute.amazonaws.com/descriptions" --type csv --collection shoeslist --headerline --file /Users/srujan/SSB-description-SDC/shoeData.csv
